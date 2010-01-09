d762fc299d9cb607e0cb10e79b82f82c5860777b
Todd Sundsted <todd@sundsted.com>
2010-01-09 00:02:10 -0500

    Added tld and split methods.

--- lib/public_suffix_list.rb
+++ lib/public_suffix_list.rb
@@ -26,7 +26,38 @@ class PublicSuffixList
   end
 
   def initialize(url = self.class.config.effective_tld_names_url)
-    Parser.parse(open(url))
+    @rules = Parser.parse(open(url))
+  end
+
+  def split(domain)
+    domain = domain.split(".")
+    result = best(match(domain.dup, @rules))
+    [domain.dup.reverse.drop(result.size + 1).reverse.join("."), domain.dup.reverse.drop(result.size).first, result.reverse.join('.')]
+  end
+
+  def tld(domain)
+    best(match(domain.split("."), @rules)).reverse.join(".")
+  end
+
+  private
+
+  def match(domain, rules)
+    return [] if domain.empty? or rules.empty?
+    set = []
+    first = domain.pop
+    [[first, first], ["!#{first}", "!#{first}"], ["*", first]].each do |a, b|
+      if rules[a]
+        set << [b]
+        match(domain, rules[a]).each { |result| set << [first] + result }
+      end
+    end
+    set
+  end
+
+  def best(results)
+    result = results.find { |r| r.last[0] == ?! } || results.sort { |a, b| a.size <=> b.size }.last
+    result = result[0..result.size - 2] if result.last[0] == ?!
+    result
   end
 
 end
