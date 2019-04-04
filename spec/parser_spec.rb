24182f0a6de1b6d14397bd5cd9b343759a24a794
Todd Sundsted <todd@sundsted.com>
2019-04-04 06:05:37 -0400

    Add explicit tests for the parser.

---
+++ spec/parser_spec.rb
@@ -0,0 +1,40 @@
+require 'public_suffix_list'
+
+describe PublicSuffixList do
+
+  describe ".parse" do
+
+    context "rules is empty" do
+      it "returns the terminal" do
+        PublicSuffixList::Parser.parse([]).should eq({})
+      end
+    end
+
+    it "parses rules" do
+      PublicSuffixList::Parser.parse(["com"]).should eq({"com" => {:term => true}})
+      PublicSuffixList::Parser.parse(["com", "foo.com"]).should eq({"com" => {"foo" => {:term => true}, :term => true}})
+      PublicSuffixList::Parser.parse(["foo.com", "com"]).should eq({"com" => {"foo" => {:term => true}, :term => true}})
+      PublicSuffixList::Parser.parse(["foo.com", "bar.com"]).should eq({"com" => {"foo" => {:term => true}, "bar" => {:term => true}}})
+      PublicSuffixList::Parser.parse(["*.jp"]).should eq({"jp" => {"*" => {:term => true}}})
+      input = %w[
+        com
+        *.jp
+        *.hokkaido.jp
+        *.tokyo.jp
+        !pref.hokkaido.jp
+        !metro.tokyo.jp
+      ]
+      output = {
+        "com" => {:term => true},
+        "jp" => {
+          "tokyo" => {"!metro" => {:term => true}, "*" => {:term => true}},
+          "hokkaido" => {"!pref" => {:term => true}, "*" => {:term => true}},
+          "*" => {:term => true}
+        }
+      }
+      PublicSuffixList::Parser.parse(input).should eq(output)
+    end
+
+  end
+
+end
